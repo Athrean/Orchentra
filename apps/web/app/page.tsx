ca7fed2b7062eae0a3d788f224518a9719a3c6f6
Rishit Sharma <rishitsharmar89@gmail.com>
2026-05-21 11:18:10 +0530

    feat(web): add FeatureTriptych landing section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 // apps/web/app/page.tsx
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { DitherGrid, ExecutionGraphDemo, Hero } from '../components/marketing-v2'
+import { DitherGrid, ExecutionGraphDemo, FeatureTriptych, Hero } from '../components/marketing-v2'
 import { getApiBase, getLoginUrl } from './lib/get-login-url'
 
 export default async function Page(): Promise<React.ReactNode> {
@@ -33,6 +33,7 @@ export default async function Page(): Promise<React.ReactNode> {
       <DitherGrid />
       <Hero loginHref={loginUrl} />
       <ExecutionGraphDemo />
+      <FeatureTriptych />
     </main>
   )
 }
